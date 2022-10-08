ad3c761ef5b43f88b39781500b2e65afeb56f54e
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-08 20:47:54 +0600

    component added and outlet

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/orders">Orders</Link>
+        </div>
+    );
+};
+
+export default Header;
